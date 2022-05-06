2c1c1d712dc75a8cb665acc2936706c9e2f48434
KrispTea <99702166+KrispTea@users.noreply.github.com>
2022-05-05 22:48:28 -0500

    Update objects.py

--- objects.py
+++ objects.py
@@ -12,7 +12,7 @@ class Dorito(pygame.sprite.Sprite):
         self.direction = Vector2()
 
     def physics(self):
-        y = random.randint(50, 600)
+        y = random.randint(50, 500)
         self.rect.center += self.direction
         self.direction.x = -10
         
@@ -24,20 +24,26 @@ class Dorito(pygame.sprite.Sprite):
         #pygame.draw.rect(self.displaySurface, ('black'), self.rect, 1)
         self.physics()
 
-# Double inheritance?????
-class Clouds(Dorito):
+class Clouds(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
 
-        self.image = pygame.image.load('images/dorito.png').convert_alpha()
-        self.image = pygame.transform.scale(self.image, (125, 125))
+        self.image = pygame.image.load('images/cloud.png').convert_alpha()
+        #self.image = pygame.transform.scale(self.image, (125, 125))
         self.rect = self.image.get_rect(topleft = pos)
         self.direction = pygame.math.Vector2()
+        self.image.set_alpha(100)
 
     def physics(self):
-        pass
+        y = random.randint(50, 500)
+        self.rect.center += self.direction
+        self.direction.x = -3
+        
+        if self.rect.centerx <= -100:
+            self.rect.centerx = 1400
+            self.rect.centery = y
 
     def update(self):
-        pass
+        self.physics()
 
 
